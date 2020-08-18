95a7519f609aa2ec4f4371c286df8b10b6ebcd48
David <dsi28@hotmail.com>
2020-08-18 00:01:56 -0500

    add redux-persist so that the redux state can persist using local storage

--- redux/configureStore.js
+++ redux/configureStore.js
@@ -1,15 +1,24 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { persistStore, persistCombineReducers } from 'redux-persist'; // these function functions will add persistance support to the reducers so that they  will update the state to persite storage whenever there is a change to the state
+import storage from 'redux-persist/es/storage'; //give access to local storage on device
 import { campsites } from './campsites';
 import { comments } from './comments';
 import { promotions } from './promotions';
 import { partners } from './partners';
 import { favorites } from './favorites';
 
+const config = {
+    key: 'root',
+    storage,
+    debug: true
+}
+
 export const ConfigureStore = () => {
     const store = createStore(
-        combineReducers({
+        //persistCombineReducers will update the state to local storage whenever the a reducer is used to update store
+        persistCombineReducers(config, {
             campsites,
             comments,
             partners,
@@ -18,5 +27,8 @@ export const ConfigureStore = () => {
         }),
         applyMiddleware(thunk)
     );
-    return store;
+
+    const persistor = persistStore(store);//enabled the store to be peristed
+
+    return {persistor,store};
 }
