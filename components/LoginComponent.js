5be05447dc9e98ce89f065dbe6cbcb627253960d
David <dsi28@hotmail.com>
2020-08-29 12:11:40 -0500

    make changes to image. get image from gallary

--- components/LoginComponent.js
+++ components/LoginComponent.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, ScrollView, Image } from 'react-native';
+import { View, StyleSheet, ScrollView, Image, CameraRoll } from 'react-native';
 import { Input, CheckBox, Button, Icon } from 'react-native-elements';
 import * as SecureStore from 'expo-secure-store';
 import * as ImagePicker from 'expo-image-picker';
 import * as Permissions from 'expo-permissions';
 import { createBottomTabNavigator } from 'react-navigation';
+import * as ImageManipulator from 'expo-image-manipulator';
+//import * as MediaLibrary from 'expo-media-library';
+
 import { baseUrl } from '../shared/baseUrl';
 
 class LoginTab extends Component {
@@ -136,12 +139,35 @@ class RegisterTab extends Component {
             });
             if(!captureImage.cancelled){
                 console.log(captureImage);
-                this.setState({imageUrl: captureImage.uri})
+                //await MediaLibrary.saveToLibraryAsync(captureImage.uri);
+                CameraRoll.saveToCameraRoll(captureImage.uri);
+                this.processImage(captureImage.uri);
             }
         }
-
-
     } 
+
+    processImage = async (imgUri) => {
+        const processedImage = await ImageManipulator.manipulateAsync(imgUri, [
+            {resize:{ width: 400}}
+        ],
+        {format: ImageManipulator.SaveFormat.PNG });
+        console.log(processedImage);
+        this.setState({imageUrl: processedImage.uri});
+    }
+
+    getImageFromGallery = async () => {
+        const cameraRollPermissions = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if(cameraRollPermissions.status === 'granted'){
+            const capturedImage = await ImagePicker.launchImageLibraryAsync(
+                {allowsEditing:true,
+                aspect:[1,1]
+            });
+            if(!capturedImage.cancelled){
+                console.log(capturedImage);
+                this.processImage(capturedImage.uri);
+            }
+        }
+    }
 
     handleRegister = ()  => {
         console.log(JSON.stringify(this.state));
@@ -178,6 +204,10 @@ class RegisterTab extends Component {
                         <Button
                             title='Camera'
                             onPress={this.getImageFromCamera}
+                        />
+                        <Button
+                            title='Gallery'
+                            onPress={this.getImageFromGallery}
                         />
                     </View>
                     <Input
